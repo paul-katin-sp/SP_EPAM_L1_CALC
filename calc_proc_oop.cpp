50c6c592bf99315b08daa9a0a5619b8f9c66b1a0
paul-katin-sp <PavelKatin@tutanota.com>
2021-09-14 11:11:03 +0000

    ddd

---
+++ calc_proc_oop.cpp
@@ -0,0 +1,22 @@
+#include "calc_proc_oop.h"
+
+void main_calc()
+{
+
+}
+void main_action_add(int OperandX, int OperandY)
+{
+    
+}
+void main_action_sub(int OperandX, int OperandY)
+{
+    
+}
+void main_action_mult(int OperandX, int OperandY)
+{
+    
+}
+void main_action_dev(int OperandX, int OperandY)
+{
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,41 +1,16 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+#include "calc_proc_oop.h"
 //============================================================================
 // Name        : main for EPAM calk
 // Authors     : Paul Katin and [1]
 // Version     : 0.1
 // Copyright   : MIT
-// Description : Calk. first stage   C++! siple algoritm Impl. For lect.1
-// Created on  :  0.1 ed. 12 2021 г
+// Description : Calk. first stage   C++! Proc implam in CPP tuls For lect.1
+// Created on  :  0.1 ed. 14.09  2021 г
 //============================================================================
-
-
 int main(int argc, char * argv []){
-    int OperandX, OperandY,Rezult, Continue;
-    char Operator;
-  do{
-    cout << "Please, input arifm. operator(+,-,*,/): "<< endl;
-    cin >> Operator;
-
-    cout << "Please, input first Operand"<< endl;
-    cin >> OperandX;
-
-    cout << "Please, input second Operand"<< endl;
-    cin >> OperandY;
-
-        if (Operator=='+'){Rezult=OperandX+OperandY;}
-
-        else if (Operator=='-'){Rezult=OperandX-OperandY;}
-
-        else if (Operator=='*'){Rezult=OperandX*OperandY;}
-
-        else if (Operator=='/'){Rezult=OperandX/OperandY;}
-
-    cout << "Result is "<<Rezult<<endl;
-    cout << "For continue put 1 ";
-    cin >>Continue;
-    }while(Continue==1);
-
-    cout << "Program has finished. Put any key";
-    cin >>Continue;
+   calc_action();
+   return 0;
+  
     }
