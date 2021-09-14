cd738fc7a222bb7ad6dbf4b0247974e032751036
paul-katin-sp <PavelKatin@tutanota.com>
2021-09-14 10:22:12 +0000

    Calc for l1 prock impl. with C== tools

---
+++ calc_proc_oop.cpp
@@ -0,0 +1,59 @@
+#include "calc_proc_oop.h"
+
+//-------
+int calc_action_plus(int OperandX, int OperandY)
+{
+    return OperandX+OperandY;
+}
+//---------
+int calc_action_sb(int OperandX, int OperandY)
+{
+    return OperandX-OperandY;
+}
+//-------
+int calc_action_mult(int OperandX, int OperandY)
+{
+    return OperandX*OperandY;
+}
+//------
+int calc_action_div(int OperandX, int OperandY)
+{
+    if (OperandY==0)
+    {
+        cout<<"Is Ziro dev ! Do not action and wrong result!";
+        return 1;
+    } else 
+        return OperandX/OperandY;
+}
+//--------
+void calc_action(){
+     int OperandX, OperandY,Rezult, Continue;
+    char Operator;
+    int Result;
+    do{
+        cout << "Please, input arifm. operator(+,-,*,/): "<< endl;
+        cin >> Operator;
+
+        cout << "Please, input first Operand"<< endl;
+        cin >> OperandX;
+
+        cout << "Please, input second Operand"<< endl;
+        cin >> OperandY;
+
+            if (Operator=='+'){Rezult=calc_action_plus(iOperandX,OperandY);}
+
+            else if (Operator=='-'){Rezult=calc_action_sb(iOperandX,OperandY);}
+
+            else if (Operator=='*'){Rezult=calc_action_mult(iOperandX,OperandY);}
+
+            else if (Operator=='/'){Rezult=calc_action_div(iOperandX,OperandY);}
+
+        cout << "Result is "<<Rezult<<endl;
+        cout << "For continue put 1 ";
+        cin >>Continue;
+        }while(Continue==1);
+
+        cout << "Program has finished. Put any key";
+        cin >>Continue;
+        return;
+}
---
+++ calc_proc_oop_.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void calc_action();
+int calc_action_plus(int OperandX, int OperandY);
+int calc_action_mn(int OperandX, int OperandY);
+int calc_action_mult(int OperandX, int OperandY);
+int calc_action_div(int OperandX, int OperandY);
+        
